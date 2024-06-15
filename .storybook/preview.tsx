ccd6506ff3c6cba6e2261b36d34e8ee2815bf7c9
tooooofu24 <toya24xxx@i.softbank.jp>
2024-06-15 16:37:24 +0900

    chore: storybookの導入

---
+++ .storybook/preview.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ChakraProvider>
+        <Story />
+      </ChakraProvider>
+    ),
+  ],
+};
+
+export default preview;
